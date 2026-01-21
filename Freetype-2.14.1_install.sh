a5b46985b478e7160f16deea463cdd930fbc64be
Nicole Labra Avila <64498975+NicoleLabrAvila@users.noreply.github.com>
2026-01-21 18:36:47 +0000

    Fix syntax error in package_archive variable

--- Freetype-2.14.1_install.sh
+++ Freetype-2.14.1_install.sh
@@ -21,7 +21,7 @@ _env_setup() {
     package_version="2.14.1"
     unpack_dir="freetype-2.14.1"
    
-    package_archive="$/shared/ucl/apps/$package_name}/installers/"
+    package_archive="$/shared/ucl/apps/${package_name}/installers/"
     #package_archive="${HOME}/${unpack_dir}"
     mkdir -p ${package_archive}
     
