767fce5abb933ca8b5c25c8c9914e2503f8c2727
Nicole Labra Avila <64498975+NicoleLabrAvila@users.noreply.github.com>
2026-02-24 19:41:19 +0000

    Update R_packages_UCL_3_4.5.1.R

--- files/R_UCL/R_packages_UCL_3_4.5.1.R
+++ files/R_UCL/R_packages_UCL_3_4.5.1.R
@@ -28,8 +28,8 @@ repros;
 
 # Removed from CRAN for R 3.4.0 - use RNetCDF
 
-install.packages ("RNetCDF", lib=mainLib, repos=repros);
-
+netcdf2Conf <- '--with-udunits2-include=/shared/ucl/apps/NetCDF/4.9.2-intel-2022/include --with-udunits2-lib=/shared/ucl/apps/NetCDF/4.9.2-intel-2022/lib';
+install.packages ("RNetCDF", lib=mainLib, repos=repros, configure.args=netcdf2Conf);
 
 # For Dr Jiaqiu Wang (jiaqiu.wang@ucl.ac.uk) (ucesjw0) - added March 2013
 
