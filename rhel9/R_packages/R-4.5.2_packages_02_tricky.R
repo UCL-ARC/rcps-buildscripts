2309636884bb297e6a466e9811ace6921a9c9114
Matthew Gorby <m.gorby@ucl.ac.uk>
2026-03-10 15:36:37 +0000

    Update R-4.5.2_packages_02_tricky.R

--- rhel9/R_packages/R-4.5.2_packages_02_tricky.R
+++ rhel9/R_packages/R-4.5.2_packages_02_tricky.R
@@ -33,9 +33,8 @@ install.packages ("classInt", lib=mainLib, repos=repros);
 install.packages ("tmap", lib=mainLib, repos=repros);
 
 # needs units, udunits2
-# sf is installed in a seaparate R script
 install.packages ("spdep", lib=mainLib, repos=repros);
-#install.packages ("sf", lib=mainLib, repos=repros);
+install.packages ("sf", lib=mainLib, repos=repros);
 
 # lot of dependencies inc graphics libraries
 install.packages ("tidyverse", lib=mainLib, repos=repros);
@@ -43,15 +42,27 @@ install.packages ("tidyverse", lib=mainLib, repos=repros);
 # depends on gmp, gmp needs to know where header file is
 # it won't properly pick up the ld and c paths so they need to be defined in ~/.R/Makevars
 install.packages("gmp", lib=mainLib, repos=repros);
-install.packages("Rmpfr", lib=mainLib, repos=repros);
-install.packages ("GJRM", lib=mainLib, repos=repros);
+
+RmpfrConf <- '--with-mpfr-include=/lustre/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/mpfr-4.2.1-sr64udbzkumnxp4i7ptdt4org2xxhird/include --with-mpfr-lib=/lustre/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/mpfr-4.2.1-sr64udbzkumnxp4i7ptdt4org2xxhird/lib';
+install.packages("Rmpfr", lib=mainLib, repos=repros, configure.args=RmpfrConf);
+
+#install.packages ("GJRM", lib=mainLib, repos=repros);
 install.packages ("MendelianRandomization", lib=mainLib, repos=repros);
 
 # udunits, sf, jqr, protolite
+Sys.setenv(INCLUDE_DIR="/lustre/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/jq-1.7.1-qm4xcbnyijfrrit2ykbbm5rkhhxqnr4m/include");
+Sys.setenv(LIB_DIR="/lustre/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/jq-1.7.1-qm4xcbnyijfrrit2ykbbm5rkhhxqnr4m/lib");
+install.packages ("jqr", lib=mainLib, repos=repros);
 install.packages ("geojsonio", lib=mainLib, repos=repros);
+Sys.unsetenv("INCLUDE_DIR");
+Sys.unsetenv("LIB_DIR");
 
 # for CHLD0085 - January 2023. jpeg
+Sys.setenv(JPEG_LIBS="-L/lustre/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/libjpeg-turbo-3.0.3-72nb3zcslrdlxocfdtuwvxeiegv6oieh/lib64");
+Sys.setenv(JPEG_CFLAGS="-I/lustre/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/libjpeg-turbo-3.0.3-72nb3zcslrdlxocfdtuwvxeiegv6oieh/include");
 install.packages ("nlmixr2", lib=mainLib, repos=repros);
+Sys.unsetenv("JPEG_LIBS");
+Sys.unsetenv("JPEG_CFLAGS");
 
 # sometimes issues with Rcpp?
 install.packages ("arrow", lib=mainLib, repos=repros);
@@ -68,4 +79,3 @@ install.packages ("cmdstanr", repos = "https://stan-dev.r-universe.dev");
 
 # mysql/mariadb
 install.packages ("RMySQL", lib=mainLib, repos=repros);
-
