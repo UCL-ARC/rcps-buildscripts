69ccfee1cf48b4348fb0a515b2aa5d05c0e28396
Matthew Gorby <ccaeorb@ucl.ac.uk>
2026-03-16 12:12:24 +0000

    Updating the installation script and R files for rhel9 R package installs

--- rhel9/R_packages/R-4.5.2_packages_02_tricky.R
+++ rhel9/R_packages/R-4.5.2_packages_02_tricky.R
@@ -1,45 +1,130 @@
-# R_packages_02_tricky - run after 01
-# These packages require additional options or are just more likely to need deps tweaking
+#=====================================================================
+#  Bioconductor_UCL_1
+#
+# Install Bioconductor for UCL R installations.
+#
+# June 2016
+# Updated May 2017
+# Updated April 2019 for Bioconductor 3.8 which uses a new installation
+# tool - BiocManager also addeded IlluminaHumanMethylationEPICanno.ilm10b4.hg19
+# Updated April 2020 to remove ROOT for R 3.6.3
+# Updated November 2020 to fix install of INLA for RedHhat 7
+# Updated August 2021 to add karyoploteR
+# Updated September 2021 for R 4.1.1
+# Updated February 2023 for R 4.2.2
+# Updated March 2024 to add additional packages required by INLA.
+# Updated January 2026 to install them with R 4.5.1
 
 mainLib <- Sys.getenv ("R_LIBS_SITE");
 repros <- Sys.getenv ("REPROS");
+dbLib <- Sys.getenv ("RLIB_DB");
 mainLib;
 repros;
+dbLib;
 
-options(Ncpus = 4)
 
-# devtools - ragg depends on libwebp and other graphics libraries
-install.packages ("ragg", lib=mainLib, repos=repros);
-install.packages ("devtools", lib=mainLib, repos=repros);
+# Download the Bioconductor installer and install the base package (ignore the first error)
 
-# Gets a static build of V8 as part of install. RHEL9 works by default. Otherwise may need
-# Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1); and possibly force a source install.
-# install V8's dependencies first, otherwise v8conf variables get lost in the interim
-install.packages("Rcpp", lib=mainLib, repos=repros);
-install.packages("jsonlite", lib=mainLib, repos=repros);
-install.packages("curl", lib=mainLib, repos=repros);
-install.packages ("V8", lib=mainLib, repos=repros);
+BiocManager::install ("GenomicRanges", type="source", lib=mainLib);
+BiocManager::install ("Rsamtools", type="source", lib=mainLib);
+BiocManager::install ("BSgenome", type="source", lib=mainLib);
+BiocManager::install ("BSgenome.Hsapiens.UCSC.hg19", type="source", lib=mainLib);
+BiocManager::install ("BiocParallel", type="source", lib=mainLib);
+BiocManager::install ("GenomicAlignments", type="source", lib=mainLib);
+BiocManager::install ("rtracklayer", type="source", lib=mainLib);
+BiocManager::install ("made4", type="source", lib=mainLib);
+BiocManager::install ("graph", type="source", lib=mainLib);
+BiocManager::install ("GSEABase", type="source", lib=mainLib);
+BiocManager::install ("hugene10stprobeset.db", type="source", lib=dbLib);
+BiocManager::install ("hugene10sttranscriptcluster.db", type="source", lib=dbLib);
 
-# needs to know where udunits is
-udunits2Conf <- '--with-udunits2-include=/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/udunits-2.2.28-7k5tsjgycgmhrhdarctbt7exdxsjcxbu/include --with-udunits2-lib=/apps/spack/0.23/deploy/2025-09/spack/opt/spack/linux-rhel9-cascadelake/gcc-12.3.0/udunits-2.2.28-7k5tsjgycgmhrhdarctbt7exdxsjcxbu/lib';
-install.packages ("udunits2", lib=mainLib, repos=repros, configure.args=udunits2Conf);
+# For Somdutta Dhir (somdutta.dhir@ucl.ac.uk) - added February 2013
 
-# units also needs to know where udunits is
-install.packages ("units", lib=mainLib, repos=repros, configure.args=udunits2Conf);
+BiocManager::install ("GenomicFeatures", type="source", lib=mainLib);
 
-# rest of tmap after V8 and udunits2
-install.packages ("lwgeom", lib=mainLib, repos=repros);
-install.packages ("classInt", lib=mainLib, repos=repros);
-install.packages ("tmap", lib=mainLib, repos=repros);
+BiocManager::install ("VariantAnnotation", type="source", lib=mainLib);
+BiocManager::install ("biovizBase", type="source", lib=mainLib);
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+install.packages("interp", type="source", lib=mainLib);
+BiocManager::install ("Gviz", type="source", lib=mainLib); # Depend on interp
+BiocManager::install ("annmap", type="source", lib=mainLib);
 
-# needs units, udunits2
-install.packages ("spdep", lib=mainLib, repos=repros);
-install.packages ("sf", lib=mainLib, repos=repros);
+# For Adam Levine (a.levine@ucl.ac.uk) - added August 2013
 
-# lot of dependencies inc graphics libraries
-install.packages ("tidyverse", lib=mainLib, repos=repros);
+BiocManager::install ("impute", type="source", lib=mainLib);
+install.packages ("WGCNA", lib=mainLib, repos=repros);
 
-# depends on gmp, gmp needs to know where header file is
+# For Dr Gioia Altobelli g.altobelli@ucl.ac.uk - added Oct 2013 updated Mar 2014
+
+BiocManager::install ("edgeR", type="source", lib=mainLib);
+BiocManager::install ("BeadDataPackR", type="source", lib=mainLib);
+BiocManager::install ("illuminaio", type="source", lib=mainLib);
+BiocManager::install ("AnnotationForge", type="source", lib=dbLib);
+BiocManager::install ("illuminaHumanv4.db", type="source", lib=dbLib);
+
+# For Antonia Ford (a.ford.11@u
+BiocManager::install ("IlluminaHumanMethylation450kmanifest", type="source", lib=dbLib);
+BiocManager::install ("methylumi", type="source", lib=mainLib);
+BiocManager::install ("nleqslv", type="source", lib=mainLib);
+BiocManager::install ("lumi", type="source", lib=mainLib);
+BiocManager::install ("IlluminaHumanMethylation450k.db", type="source", lib=dbLib);
+BiocManager::install ("FDb.InfiniumMethylation.hg19", type="source", lib=mainLib);
+BiocManager::install ("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", lib=mainLib); # Added April 2019
+BiocManager::install ("ROC", type="source", lib=mainLib);
+BiocManager::install ("wateRmelon", type="source", lib=mainLib);
+install.packages ("ruv", lib=mainLib, repos=repros);
+BiocManager::install ("ChAMP", type="source", lib=mainLib); # Dependency Gviz
+
+# For Ana Paula Leite (ana.leite@ucl.ac.uk) - added June
+
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+install.packages("brio")
+BiocManager::install ("EBSeq", type="source", lib=mainLib);
+
+# For Athina Dritsoula (athina.dritsoula.09@ucl.ac.uk) - added Feb and March 2017
+
+BiocManager::install ("goseq", type="source", lib=mainLib);
+BiocManager::install ("missMethyl", type="source", lib=dbLib);
+
+# For Evgeny Galimov (e.galimov@ucl.ac.uk) - added Aug 2017
+
+BiocManager::install ("DEXSeq", type="source", lib=mainLib);
+
+# For Dean Cornish (dean.cornish.19@ucl.ac.uk) - added Aug 2021
+
+BiocManager::install ("karyoploteR", type="source", lib=mainLib);
+
+# Additional CRAN packages
+install.packages ("PSCBS", lib=mainLib, repos=repros);
+
+# INLA - updated with extra packages and to fix binary install of INLA itself with a version that will run on RedHat 7.
+# Updated March 2024 as additional R packasges needed from CRAN
+
+udunits2Conf <- '--with-udunits2-include=/shared/ucl/apps/udunits/2.2.28/include --with-udunits2-lib=/shared/ucl/apps/udunits/2.2.28/lib';
+install.packages ("fmesher", lib=mainLib, repos=repros, configure.args=udunits2Conf);
+install.packages ("Ecdat", lib=mainLib, repos=repros);
+install.packages ("mlogit", lib=mainLib, repos=repros);
+install.packages ("splancs", lib=mainLib, repos=repros);
+install.packages ("tidyterra", lib=mainLib, repos=repros,  configure.args=udunits2Conf);
+install.packages ("INLAspacetime", lib=mainLib, repos=repros);
+install.packages ("INLA", lib=mainLib, repos=c(getOption("repros"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE);
+
+# Replace binary INLA that doesn't work on redHat 7 with one that does. NOTE: interactive will prompt
+# you to choose the correct binary. - Updated Feb 2023
+
+library (INLA);
+inla.binary.install();
+
+# For Elizabeth Boyse (ucbtea5@ucl.ac.uk) - added on 10/02/2026
+
+BiocManager::install ("dada2", type="source", lib=mainLib);
+
+# End of Bioconductor_UCL_1
+
+# BiocManager::install ("XXX", type="source", lib=mainLib);
+
+[ccspapp@login21 r-4.5.2_tmp]$ cat R-4.5.2_packages_02_tricky.R
+
 # it won't properly pick up the ld and c paths so they need to be defined in ~/.R/Makevars
 install.packages("gmp", lib=mainLib, repos=repros);
 
@@ -79,3 +164,4 @@ install.packages ("cmdstanr", repos = "https://stan-dev.r-universe.dev");
 
 # mysql/mariadb
 install.packages ("RMySQL", lib=mainLib, repos=repros);
+
